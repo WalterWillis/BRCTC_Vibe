738308f56c5770cb63a40323e239487698cac4ba
Walter Willis <waltertwillis@outlook.com>
2018-06-13 16:42:51 -0400

    Removed Gyro functionality for launch. Pins were not compaitble with the board.

--- Project1/Thread1Code.cpp
+++ Project1/Thread1Code.cpp
@@ -42,7 +42,7 @@ void *SpiDataCollector(void *unused) { //import time now and start + 30
 
 	const int length = 30000;
 	const int ADC_Pins = 3;
-	const int Gyro_DataPoints = 10; 
+	//const int Gyro_DataPoints = 10; 
 
 
 	////ADC SETUP
@@ -50,8 +50,8 @@ void *SpiDataCollector(void *unused) { //import time now and start + 30
 	PiHatHeader(ADC.fileName); // file name is in the .h file
 	
 	////GYRO SETUP
-	ADIS16460 Gyro;
-	GyroHeader(Gyro.fileName);	
+	//ADIS16460 Gyro;
+	//GyroHeader(Gyro.fileName);	
 
 	//Gyro.RegWrite(MSC_CTRL, 0xC1); //Tells Gryo to enable Data Ready, and sets the polarity according to the ADIS16460 example (See .cpp file) 
 	//delay(20);
@@ -60,7 +60,7 @@ void *SpiDataCollector(void *unused) { //import time now and start + 30
 	//Gyro.RegWrite(DEC_RATE, 0); //Disables decimation
 	//delay(20);
 
-	const int pinDR = 2; // Data Ready pin on RPI
+	//const int pinDR = 2; // Data Ready pin on RPI
 
 
 	////BEGIN
@@ -71,8 +71,8 @@ void *SpiDataCollector(void *unused) { //import time now and start + 30
 		double realResults[length][ADC_Pins];
 
 		////GYRO
-		int16_t *burstData[length][Gyro_DataPoints];
-		int16_t burstChecksum = 0; // used to verify checksum
+		//int16_t *burstData[length][Gyro_DataPoints];
+		//int16_t burstChecksum = 0; // used to verify checksum
 
 		////Time
 		time_t timeData[length];
@@ -87,8 +87,8 @@ void *SpiDataCollector(void *unused) { //import time now and start + 30
 				realResults[i][pin] = (double)result / (double)4095 * ADC.vref;
 			}
 			
-			burstData[i] = Gyro.burstRead();
-			delayMicroseconds(16); // according to the datasheet, this is the appropriate delay between readings. The ADC read process is a timing blackbox.
+			//burstData[i] = Gyro.burstRead();
+			//delayMicroseconds(16); // according to the datasheet, this is the appropriate delay between readings. The ADC read process is a timing blackbox.
 		}
 
 		ofstream myfile;
@@ -104,29 +104,29 @@ void *SpiDataCollector(void *unused) { //import time now and start + 30
 		myfile.flush();
 		myfile.close();
 
-		myfile.open("GYRO_TEST.txt", ios::out | ios::app);
-		if (myfile.is_open())
-		{
-			for (int i = 0; i < length; i++) {
-				for (int j = 0; j < Gyro_DataPoints; j++)
-				{
-					char semi = ';';
-					if (j == 0) // Redundant with else, but 0 will always be the first selection hit. May need to be optimized.
-						myfile << burstData[i][j] << semi;
-					else if (j > 0 && j <= 3)
-						myfile << Gyro.gyroScale(*(burstData[i][j])) << semi;
-					else if (j > 3 && j <= 6)
-						myfile << Gyro.accelScale(*(burstData[i][j])) << semi;
-					else if (j == 7)
-						myfile << Gyro.tempScale*(burstData[i][j])) << semi;
-					else
-						myfile << burstData[i][j] << semi;
-				}
-				myfile << Gyro.checksum(burstData[i]) << timeData[i] << endl;
-			}			
-		}
-		myfile.flush();
-		myfile.close();
+		//myfile.open("GYRO_TEST.txt", ios::out | ios::app);
+		//if (myfile.is_open())
+		//{
+		//	for (int i = 0; i < length; i++) {
+		//		for (int j = 0; j < Gyro_DataPoints; j++)
+		//		{
+		//			char semi = ';';
+		//			if (j == 0) // Redundant with else, but 0 will always be the first selection hit. May need to be optimized.
+		//				myfile << burstData[i][j] << semi;
+		//			else if (j > 0 && j <= 3)
+		//				myfile << Gyro.gyroScale(*(burstData[i][j])) << semi;
+		//			else if (j > 3 && j <= 6)
+		//				myfile << Gyro.accelScale(*(burstData[i][j])) << semi;
+		//			else if (j == 7)
+		//				myfile << Gyro.tempScale*(burstData[i][j])) << semi;
+		//			else
+		//				myfile << burstData[i][j] << semi;
+		//		}
+		//		myfile << Gyro.checksum(burstData[i]) << timeData[i] << endl;
+		//	}			
+		//}
+		//myfile.flush();
+		//myfile.close();
 
 	}
 	pthread_exit(NULL);
